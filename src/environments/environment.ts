904e4b277ec13cb59d9144992256a8c3bfe19d77
@GeovannyAlExRs <axel94bsc@gmail.com>
2022-12-02 16:22:38 -0500

    Add Firebase to Angular

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBUvEXEpTLlEfUKtoJbm9YogrSMbpuokiI",
+        authDomain: "employmentdtk.firebaseapp.com",
+        projectId: "employmentdtk",
+        storageBucket: "employmentdtk.appspot.com",
+        messagingSenderId: "298129167127",
+        appId: "1:298129167127:web:6ed45e4b3a61ae8db35d52"
+    }
+};
